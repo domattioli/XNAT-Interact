e9468383d9e1646cd00c6cc38676ed0fd96985d5
domattioli <dominik-mattioli@uiowa.edu>
2024-08-07 12:34:40 -0500

    minor edit to user prompt for input init

--- src/xnat_resource_data.py
+++ src/xnat_resource_data.py
@@ -88,7 +88,7 @@ class ORDataIntakeForm( ResourceFile ):
 
     def _read_from_file( self, parent_folder: str, verbose: Opt[bool]=False ) -> None:
         ffn = os.path.join( parent_folder, self.filename_str )
-        if verbose: print( f'\n\t--- Initializing IntakeFrom from {ffn} ---' )
+        if verbose: print( f'\n\t...Initializing OR Intake From from "{ffn}"...' )
         with open( ffn, 'r', encoding='cp1252' ) as jf:
             self._running_text_file = json.loads( jf.read() )
 
